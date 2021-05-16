900179c31902aa3765c3da1e6425de2850e1ce58
tskoxid <rus21sia@gmail.com>
2021-05-16 23:55:01 +0300

    add files

---
+++ main.py
@@ -0,0 +1,21 @@
+from pdf_text_extraction import PdfParser
+
+
+def main(url, count):
+    start_time = time.time()
+    try:
+        parser.pdf_pipeline(url, count)
+    except Exception as e:
+        print(e)
+    end_time = time.time() - start_time
+    print(f'----All time is %.5f seconds---- \n' % end_time)
+
+
+if __name__ == "__main__":
+    URL_PDF = [r'https://arxiv.org/pdf/1909.11687.pdf', 'https://arxiv.org/pdf/2103.15691.pdf',
+               'https://arxiv.org/pdf/1706.03762.pdf']
+    parser = PdfParser()
+    import time
+
+    for COUNT, URL in enumerate(URL_PDF):
+        main(URL, COUNT)
